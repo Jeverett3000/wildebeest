7a90e65ace532e4161162ffa7da91c634080ece1
Dario Piotrowicz <dario@cloudflare.com>
2023-01-07 20:17:25 +0000

    more tweaks

--- frontend/src/routes/explore/layout.tsx
+++ frontend/src/routes/explore/layout.tsx
@@ -40,7 +40,7 @@ export default component$(() => {
 	]
 
 	return (
-		<div class="explore-wrapper">
+		<div class="explore-wrapper flex flex-col">
 			<StickyHeader>
 				<h2 class="text-reg text-md m-0 p-4 bg-slate-700 xl:rounded-t">
 					<i class="fa fa-hashtag fa-fw mr-3" />
@@ -48,7 +48,7 @@ export default component$(() => {
 				</h2>
 			</StickyHeader>
 			<div class="bg-slate-900 flex justify-around">{links.map((link) => renderNavLink(link))}</div>
-			<div class="explore-content-wrapper">
+			<div class="flex-auto">
 				<Slot />
 			</div>
 		</div>
